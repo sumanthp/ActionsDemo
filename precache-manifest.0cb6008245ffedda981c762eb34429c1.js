a2097e5a11bb43d876d098c9fa4cbb3eb4aa4488
sumanthp <sumanthp@users.noreply.github.com>
2020-05-12 18:57:50 +0000

    deploy: d2b5028792442dcee18c446ac8496ff59141e9ef

---
+++ precache-manifest.0cb6008245ffedda981c762eb34429c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17ccfcfc51a419d609bfed5e65cd645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49bb01ae44420393f8ee",
+    "url": "/static/css/main.46a5275e.chunk.css"
+  },
+  {
+    "revision": "a4a9f3713c9418816449",
+    "url": "/static/js/2.dc8cb172.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc8cb172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bb01ae44420393f8ee",
+    "url": "/static/js/main.c117b700.chunk.js"
+  },
+  {
+    "revision": "fa0af807e30880911a9d",
+    "url": "/static/js/runtime-main.28faca3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
