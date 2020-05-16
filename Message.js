619c3b2188c3ca546fc998c214b70682075aa198
polisetty sumanth <sumanth472@gmail.com>
2020-05-16 16:37:42 +0530

    Change in Message.Js

---
+++ Message.js
@@ -0,0 +1,28 @@
+//using State
+
+import React, { Component } from 'react'
+class Message extends Component{
+    constructor(){
+        super()
+        this.state={
+            message : 'Welcome to Github Pages'
+        }
+    }
+    changeMessage(){
+        this.setState({
+            message: 'Thank you for Visiting'
+        })
+    }
+    render(){
+        return (
+            <div>
+                <h1>
+                { this.state.message }
+                </h1>
+                <button onClick={()=>this.changeMessage()}>Show Message</button>
+            </div>
+        )
+    }
+}
+
+export default Message
